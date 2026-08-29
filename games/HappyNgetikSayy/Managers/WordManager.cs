33bba8024c269a4db60f8d98a8df671ede8e40cc
Ibrahim Zaky M <ekaari270419@gmail.com>
2026-08-29 21:18:38 +0700

    feat: dominate HTS word list with sadboy/alay words

--- games/HappyNgetikSayy/Managers/WordManager.cs
+++ games/HappyNgetikSayy/Managers/WordManager.cs
@@ -7,23 +7,14 @@ public static class WordManager
     private static Random _random = new Random();
     private static string[] _words = new string[]
     {
-        "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon",
-        "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli", "watermelon",
-        "house", "mouse", "blouse", "spouse", "douse", "grouse", "louse", "rouse", "souse", "arouse",
-        "water", "earth", "fire", "wind", "heart", "planet", "galaxy", "universe", "star", "moon",
-        "sun", "cloud", "rain", "snow", "storm", "thunder", "lightning", "breeze", "tornado", "hurricane",
-        "computer", "keyboard", "mousepad", "monitor", "speaker", "printer", "scanner", "router", "modem", "cable",
-        "software", "hardware", "network", "internet", "website", "browser", "database", "server", "client", "protocol",
-        "algorithm", "variable", "function", "object", "class", "method", "property", "event", "delegate", "interface",
-        "abstract", "virtual", "override", "sealed", "static", "public", "private", "protected", "internal", "readonly",
-        "string", "integer", "boolean", "double", "float", "decimal", "character", "array", "list", "dictionary",
-        "dog", "cat", "bird", "fish", "turtle", "rabbit", "hamster", "guineapig", "parrot", "goldfish",
-        "lion", "tiger", "bear", "elephant", "giraffe", "zebra", "monkey", "gorilla", "kangaroo", "koala",
-        "school", "college", "university", "student", "teacher", "professor", "classroom", "library", "laboratory", "gymnasium",
+        // Beberapa kata original
+        "osmis", "santri", "ngetik", "coding", "fokus", "target", "waktu", "sahabat",
+        // Dominasi kata-kata alay / sadboy
         "merelakan", "terluka", "tulus", "tersakiti", "pahlawan", "pembunuh", "si paling effort", "disia-siakan", "diabaikan",
         "ultraman", "wong tulus", "setia", "badut", "kesatria", "pengorbanan", "ditinggalkan", "bertahan", "menyerah", "kecewa",
         "harapan", "palsu", "luka", "air mata", "sendiri", "kesepian", "hancur", "kenangan", "melupakan", "move on", "sakit",
-        "ikhlas", "menunggu", "bayangan", "hilang", "pergi", "tanpa alasan", "cinta", "rahasia", "kisah", "akhir"
+        "ikhlas", "menunggu", "bayangan", "hilang", "pergi", "tanpa alasan", "cinta", "rahasia", "kisah", "akhir",
+        "galau", "patah hati", "lupa", "menangis", "senyum palsu"
     };
 
     public static string GetRandomWord()
